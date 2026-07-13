ecd0904d9b22df33d60a5ef18e678be0d70bc856
kokifhhh123 <hxchen.cs@gmail.com>
2026-07-13 14:40:57 +0800

    complete basic UART Bootloader, tested on raspberry pi

--- lab2/bootloader/boot.c
+++ lab2/bootloader/boot.c
@@ -2,27 +2,79 @@
 
 void uart_send_size(unsigned int size) {
     unsigned char *ptr = (unsigned char *)&size;
+    uart_send_string("2 start send number\n");
     uart_send(ptr[0]);
     uart_send(ptr[1]);
     uart_send(ptr[2]);
     uart_send(ptr[3]);
+    uart_send_string("2 end send number\n");
+}
+
+void complete_sending() {
+    uart_send_string("kernel loaded! start jumping...\n");
+    return;
+}
+
+static void wait_for_magic(void)
+{
+    const unsigned char magic[4] = {'K', 'E', 'R', 'N'};
+    unsigned int matched = 0;
+    while (matched < 4) {
+        unsigned char c = uart_recv();
+        if (c == magic[matched]) {
+            matched++;
+        } else {
+            matched = 0;
+            if (c == magic[0]) {
+                matched = 1;
+            }
+        }
+    }
 }
 
 int main() {
     uart_init();
     // uart_send_string("gg\n");
+    // uart_send_string("READY_SIZE\n");
 
     unsigned int size = 0;
+    // char *tmp = (char*)0x20000;
+    // int corr = 0;
+    // int i = 0;
+    // while(1) {
+    //     tmp[i] = uart_recv();
+    //     if(i>=3) {
+    //         if(tmp[i-3]=='K' && tmp[i-1]=='E' && tmp[i-2]=='R' && tmp[i-3]=='N') {
+    //             break;
+    //         }
+    //     }
+    //     i++;
+    // }
+    wait_for_magic();
+    // uart_send_string("READY\n");
     size |= (unsigned int)uart_recv()<<0;
     size |= (unsigned int)uart_recv()<<8;
     size |= (unsigned int)uart_recv()<<16;
     size |= (unsigned int)uart_recv()<<24;
+    // size = 2278;
+
+    // uart_send_string("READY\n");
+
+    unsigned char *ptr = (unsigned char *)&size;
+    uart_send(ptr[0]);
+    uart_send(ptr[1]);
+    uart_send(ptr[2]);
+    uart_send(ptr[3]);
     
-    // char *kernel_dest = (char*)0x80000;
-    // for (unsigned int i=0; i<size; i++) {
-    //     kernel_dest[i] = uart_recv();
-    // }
+    wait_for_magic();
+    char *kernel_dest = (char*)0x20000;
+    for (unsigned int i=0; i<size; i++) {
+        kernel_dest[i] = uart_recv();
+        // kernel_dest[i] = 0;
+    }
+    // // complete_sending();
+    uart_send_string("BOOT");
     // uart_send_string("kernel loaded! start jumping...\n");
-    uart_send_string("gg\n");
-    uart_send_size(size);
+    return 0;
+    // uart_send_string("gg\n");
 }
--- lab2/loadimg/loadimg.py
+++ lab2/loadimg/loadimg.py
@@ -1,35 +1,114 @@
 import struct
 import os
 import serial
+import time
 
-def send_img():
+def test():
     kernel_path = "../kernel8.img"
-    ser = serial.Serial("/dev/pts/7", 115200)
-    size = os.path.getsize(kernel_path)
-
-    print(size)
-    ser.write(struct.pack("<I", size))
-    # ser.write(0xe6)
-    print(struct.pack("<I", size))
-
+    # ser = serial.Serial("/dev/pts/7", 115200)
+    ser = serial.Serial("/dev/ttyUSB1", 115200)
     while(1):
         if ser.in_waiting > 0:
             print(ser.read(4))
 
-    # with open(kernel_path, "rb") as f:
-    #     ser.write(f.read())
+    while(1):
+        if serial.in_waiting:
+            response = ser.readline().decode("utf-8", errors="ignore")
+            if "Jumping" in response:
+                break
+    ser.close()
+    if ser.in_waiting > 0:
+        raw_data = ser.readline()
+        text = raw_data.decode("utf-8", errors="ignore")
+        print(text.strip())
+
+def wait_for_magic(ser, magic: bytes) -> None:
+    matched = 0
+    while matched < len(magic):
+        byte = ser.read(1)
+        print(byte, byte[0])
+        print(magic, magic[matched])
+        if not byte:
+            raise TimeoutError(f"Timeout waiting for magic {magic!r}")
+        if byte[0] == magic[matched]:
+            matched += 1
+        else:
+            matched = 1 if byte[0] == magic[0] else 0
+
+def send_img():
+    kernel_path = "../kernel8.img"
+    
+    # ser = serial.Serial("/dev/pts/10", 115200, )
+    ser = serial.Serial("/dev/ttyUSB1", 115200)
+
+    size = os.path.getsize(kernel_path)
+    # ser.write(b"KERN")
+    # while True:
+    #     line = ser.readline()
+    #     print(line)
+
+    #     if b"READY" in line:
+    #         break
+    
+    # ser.write(struct.pack("<I", size))
+    header = b"KERN" + struct.pack("<I", size)
+
+    print("magic:", header[:4])
+    print("size:", size)
+    print("size bytes:", header[4:8])
+    ser.write(header)
+
+    # ser.flush()
+
+    # while True:
+    #     line = ser.readline()
+    #     print(line)
+
+    #     if b"READY" in line:
+    #         break
+    
+    returned_size_raw = ser.read(4)
+    returned_size = struct.unpack("<I", returned_size_raw)[0]
+
+    print("bootloader size:", returned_size)
+    print("expected size:", size)
+
+    with open(kernel_path, "rb") as f:
+        kernel_data = f.read()
+    print(f"bootloader size: {returned_size_raw}")
+    print(f"expected size: {len(kernel_data):x}")
+
+    header = b"KERN" + kernel_data
+    ser.write(header)
+    # chunk_size = 64
+    # for offset in range(0, len(kernel_data), chunk_size):
+    #     chunk = kernel_data[offset:offset + chunk_size]
+    # # chunk = kernel_data[0:chunk_size]
+    #     ser.write(chunk)
+    #     # ser.flush()
+    #     time.sleep(0.002)
+    wait_for_magic(ser, b"BOOT")
+    # data = ser.readline()
+    # text = data.decode("utf-8", errors="ignore")
+    # print(text.strip())
 
     # while(1):
-    #     if serial.in_waiting:
-    #         response = ser.readline().decode("utf-8", errors="ignore")
-    #         if "Jumping" in response:
-    #             break
-    # ser.close()
-    # if ser.in_waiting > 0:
-    #     raw_data = ser.readline()
-    #     text = raw_data.decode("utf-8", errors="ignore")
-    #     print(text.strip())
+    #     print("waiting...")
+    #     data = ser.readline()
+    #     if data:
+    #         print("waiting... 1")
+    #         text = data.decode("utf-8", errors="ignore")
+    #         # print(data.decode(errors="replace", end=""))
+    #         print(text.strip())
+    #     if b"kernel loaded" in data:
+    #         print("waiting... 2")
+    #         print("Transfer successful")
+    #         break
+    print("done!")
+    # while(1):
+    #     if ser.in_waiting > 0:
+    #         print(ser.read(1))
 
-
+# python3 loadimg.py
 if __name__=="__main__":
     send_img()
